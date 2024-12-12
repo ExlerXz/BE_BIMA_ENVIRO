50da661d00391fcc6f2138e6780031666126ec43
ExlerXz <rajab39ali27@gmail.com>
2024-12-13 00:50:17 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -23,6 +23,17 @@ const databaseValidation = async () => {
 };
 
 module.exports = {
-  sequelize,
   databaseValidation,
-};
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+}
