75140f4030efe33f599364050b06ef16616b7887
ExlerXz <rajab39ali27@gmail.com>
2024-12-13 00:45:00 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,32 +1,28 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
+
+// Ambil DATABASE_URL dari environment variables
+const { DATABASE_URL } = process.env;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in the environment variables.");
+}
 
 // Inisialisasi Sequelize dengan connection string
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-})
+});
 
 const databaseValidation = async () => {
   try {
-    await sequelize.authenticate()
-    console.log('Success connect to database')
+    await sequelize.authenticate();
+    console.log('Success connect to database');
   } catch (err) {
-    console.error(`Unable to connect to the database: ${err}`)
+    console.error(`Unable to connect to the database: ${err}`);
   }
-}
+};
 
 module.exports = {
+  sequelize,
   databaseValidation,
-  development: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-  },
-  test: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-  },
-}
+};
